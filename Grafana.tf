6a746f837b5bce269abcaef5b38595bc5baa3a9b
haroldsphinx <adedayoakinpelu@gmail.com>
2021-11-11 15:39:27 +0100

    Integrate grafana dashboard for redis

---
+++ Grafana.tf
@@ -0,0 +1,18 @@
+data "rediscloud_database" "redis-subscription" {
+  subscription_id = rediscloud_subscription.gcp-project.id
+  name = rediscloud_subscription.gcp-project.name
+}
+
+resource "grafana_folder" "redis_cloud" {
+  title = "${var.folder_name} Redis Cloud"
+}
+
+resource "grafana_dashboard" "redis_cloud_dashboard" {
+  folder = grafana_folder.redis_cloud.id
+  config_json = templatefile(
+    "${path.module}/templates/redis-dashboard.json",
+    {
+      datasource = data.rediscloud_database.redis-subscription.public_endpoint
+    }
+  )
+}
--- main.tf
+++ main.tf
@@ -4,5 +4,9 @@ terraform {
       source  = "RedisLabs/rediscloud"
       version = ">= 0.2.4"
     }
+    grafana = {
+      source  = "grafana/grafana"
+      version = ">=1.14.0"
+    }
   }
 }
