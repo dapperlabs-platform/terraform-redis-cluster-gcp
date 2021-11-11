77d663ed2e14a0e7131354821772db1cf364c112
haroldsphinx <adedayoakinpelu@gmail.com>
2021-11-11 16:00:28 +0100

    update

--- Grafana.tf
+++ Grafana.tf
@@ -1,8 +1,3 @@
-data "rediscloud_database" "redis-subscription" {
-  subscription_id = rediscloud_subscription.gcp-project.id
-  name            = rediscloud_subscription.gcp-project.name
-}
-
 resource "grafana_folder" "redis_cloud" {
   title = "${var.folder_name} Redis Cloud"
 }
@@ -12,7 +7,7 @@ resource "grafana_dashboard" "redis_cloud_dashboard" {
   config_json = templatefile(
     "${path.module}/templates/redis-dashboard.json",
     {
-      datasource = data.rediscloud_database.redis-subscription.public_endpoint
+      datasource = var.datasource
     }
   )
 }
